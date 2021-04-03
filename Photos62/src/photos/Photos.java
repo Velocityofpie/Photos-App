4c90e0dd614868838a119d020a88c71630f86d00
Joshua <Joshuamuq@gmail.com>
2021-04-03 19:23:56 -0400

    Updating

--- Photos62/src/photos/Photos.java
+++ Photos62/src/photos/Photos.java
@@ -1,4 +1,4 @@
-package sample;
+package photos;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,12 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Photos extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("/view/sample.fxml"));
+        primaryStage.setTitle("Photo Album");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
