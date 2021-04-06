a47425c8cbdbf0292424c2e656affc2466b9bc36
Joshua <Joshuamuq@gmail.com>
2021-04-06 14:15:27 -0400

    not working?

--- Photos62/src/controller/LoginController.java
+++ Photos62/src/controller/LoginController.java
@@ -1,7 +1,8 @@
 package controller;
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.io.File;
+import java.io.FileInputStream;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,19 +13,21 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import helper.User;
 import helper.Album;
 
+
 public class LoginController {
 
-    @FXML private Button loginButton;
-    @FXML private TextField usernameTextField;
-    @FXML private TextField passwordTextField;
+    @FXML private Button loginButton , signupButton ;
+    @FXML private TextField usernameTextField, passwordTextField;
+
 
     ArrayList<User> users;
-    private final String path = "data/data.txt";
+    private final String path = "/data/data.txt";
     Boolean validUser = false;
 
     public void start(Stage stage) {
--- Photos62/src/photos/Photos.java
+++ Photos62/src/photos/Photos.java
@@ -1,22 +1,42 @@
 package photos;
 
+import controller.AdminController;
+import controller.LoginController;
+import javafx.event.ActionEvent;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
+
 
 public class Photos extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
-        primaryStage.setTitle("Photo Album");
-        primaryStage.setScene(new Scene(root, 700, 500));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/view/Login.fxml"));
+            AnchorPane root = (AnchorPane) loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.setTitle("Photos");
+            primaryStage.show();
+            LoginController controller = loader.getController();
+            controller.start(primaryStage);
+
+
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
